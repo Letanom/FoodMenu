7c151855fa7234fa7f7527a38b987b5abdd10af1
Kevın Özşimşek <58519038+Letanom@users.noreply.github.com>
2023-08-17 09:40:41 +0300

    Header.jsx
    translate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,20 +10,20 @@ const Header = ({setCategory}) => {
 
   return (
     <div>
-    <h1 className="text-5xl text-center pt-10 m-10">YEMEK MENÜSÜ</h1>
+    <h1 className="text-5xl text-center pt-10 m-10">Food Menu</h1>
   
    <div className='buttons  inline-flex py-8 px-8  min-w-full'>
    <button  className="mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={()=>setCategory("all")}>
-  Tümü
+  All
 </button>
 <button  className="mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={()=>setCategory("Kahvalti")} >
-  Kahvaltı
+  Breakfast
 </button>
 <button  className="mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={()=>setCategory("Yemek")}>
-  Yemekler
+  Foods
 </button>
 <button  className="mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={()=>setCategory("Icecek")}>
-  İçecekler
+  Drinks
 </button>
 
 
@@ -36,19 +36,3 @@ const Header = ({setCategory}) => {
 
 export default Header
 
-/*onClick={() => handleClickedT('Tumu')}
-onClick={()=> handleClicked('Kahvalti')}
-onClick={() => handleClicked('Yemek')}
-onClick={() => handleClicked('shakes')}/
-  const [group, setGroup] = useState(Foods);
-  const [activeCategory, setActiveCategory] = useState('');
-
-  const handleClicked = (category) => {
-    if (category === 'Tumu') {
-      setGroup(Foods);
-    } else {
-      const filteredGroup = Foods.filter(food => food.category === category);
-      setGroup(filteredGroup);
-    }
-    setActiveCategory(category);
-  };/*/
